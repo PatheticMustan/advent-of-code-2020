392c1d36f52facf8344ccf47084969d69c701c04
Mustan Pathetic <patheticmustan@gmail.com>
2020-12-06 13:02:05 -0500

    Create 2.js

---
+++ day 5/2.js
@@ -0,0 +1,19 @@
+function solve(input) {
+    const passes = input.split("\n");
+    
+    let seatsFilled = new Array(127*8+7).fill(" ");
+
+    for (const pass of passes) {
+        const row = +("0b" + pass.slice(0, pass.length-3).replaceAll("F", 0).replaceAll("B", 1));
+        const column = +("0b" + pass.slice(-3).replaceAll("R", 1).replaceAll("L", 0));
+        const seatID = (row * 8) + column;
+        
+        seatsFilled[seatID] = "F";
+    }
+
+    seatsFilled[seatsFilled.join("").trim().indexOf(" ")] = "M";
+
+    return seatsFilled.indexOf(" ", seatsFilled.indexOf("F"));
+}
+
+console.log(solve(`BFBBBFBLRR`));
