841fba174763a81e7afe3b1516e1377b5b9ce5f2
Mustan Pathetic <patheticmustan@gmail.com>
2020-12-10 20:47:19 -0500

    Update 2 (un).js

--- day 7/2 (un).js
+++ day 7/2 (un).js
@@ -18,22 +18,59 @@ function solve(input) {
 
         if (subBagString[0] === "no other bags") return 1;
 
-        return subBagString.map(sb => [extractNumberOfBags(sb), expand(getBag(extractSubBagName(sb)))]);
+        return subBagString.map(sb => {
+            return {
+                name: extractSubBagName(sb),
+                count: extractNumberOfBags(sb),
+                children: expand(getBag(extractSubBagName(sb)))
+            }
+        });
     }
 
-    let pancake = expand(goldenBag);
+    const bagCount = expand(goldenBag);
 
-    while (!pancake.every(v => typeof v === "number")) {
-        pancake = pancake.flat();
+    const sum = (...args) => args.reduce((a, b) => +a + +b, 0);
+
+    // root is bo, count is always Number, children is either bo[] or Number
+    // bagCount: bo = {
+    //     count: Number,
+    //     children: Number || bo[]
+    // }
+    // 
+    // to process, sum children and multiply sum by count
+    // first all children must be reduced to Number's.
+
+    const processBagObject = bagCollection => {
+        return bagCollection.map(bagObject => {
+            console.log(bagObject);
+            if (typeof bagObject.children === "number") {
+                return bagObject.children;
+            }
+
+            if (bagObject.children.every(child => child instanceof Array)) {
+                return sum(bagObject.children);
+            }
+            
+            const count = (c, h) => c * (1 + h);
+
+            return sum(bagObject.children.map(sub => {
+                const count = sub.count;
+                const subBag = processBagObject(bagObject.children);
+
+                return count*(1+sum(subBag));
+            }));
+        })
     }
-
-    return pancake.reduce((a, b) => a * b, 1);
+    
+    console.log(bagCount);
+    return processBagObject(bagCount);
 }
 
-console.log(solve(`shiny gold bags contain 2 dark red bags.
+solve(`shiny gold bags contain 2 dark red bags.
 dark red bags contain 2 dark orange bags.
 dark orange bags contain 2 dark yellow bags.
 dark yellow bags contain 2 dark green bags.
 dark green bags contain 2 dark blue bags.
 dark blue bags contain 2 dark violet bags.
-dark violet bags contain no other bags.`));
+dark violet bags contain no other bags.`)
+//console.log(solve(document.getElementsByTagName("pre")[0].textContent));
